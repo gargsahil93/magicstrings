f35e4ae4ee39e97691cdf6031140c73111730222
Sahil Garg <sgarg.awesome@gmail.com>
2020-03-14 18:23:36 +0530

    fixing exports

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
-export default {
-    add : (a, b) => {
-        if (a.length < 10 && b.length < 10) {
-            return (+a + +b).toString();
-        }
-        if (a.length < b.length) {
-            let c = a;
-            a = b;
-            b = c;
-        }
-        a = a.split("").reverse().join("");
-        b = b.split("").reverse().join("");
-        let c = '', carryover = '0';
-        [...a].forEach((ch, index) => {
-            let charB = b[index] || '0';
-            let newC = add(add(ch, charB), carryover);
-            carryover = newC.slice(0, newC.length-1);
-            c = c + newC[newC.length-1];
-        });
-        if (carryover !== 0) {
-            c = c + carryover;
-        }
-        return c.split("").reverse().join("");
+exports.add = (a, b) => {
+    if (a.length < 10 && b.length < 10) {
+        return (+a + +b).toString();
     }
+    if (a.length < b.length) {
+        let c = a;
+        a = b;
+        b = c;
+    }
+    a = a.split("").reverse().join("");
+    b = b.split("").reverse().join("");
+    let c = '', carryover = '0';
+    [...a].forEach((ch, index) => {
+        let charB = b[index] || '0';
+        let newC = add(add(ch, charB), carryover);
+        carryover = newC.slice(0, newC.length-1);
+        c = c + newC[newC.length-1];
+    });
+    if (carryover !== 0) {
+        c = c + carryover;
+    }
+    return c.split("").reverse().join("");
 }
